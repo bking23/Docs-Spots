015544a1d765e2b5563281accae0bfda332afbed
James Naurot <naurotj@gmail.com>
2021-02-20 03:02:41 -0500

    a start

--- server/server.js
+++ server/server.js
@@ -1,24 +1,103 @@
+require('dotenv').config()
 
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-require('dotenv').config();
-
 const app = express();
-const port = process.env.PORT || 5000;
+const cookieParser = require('cookie-parser')
 
-app.use(cors());
+// Google Auth
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '736729752425-puvqvdfvlhiuptbfdeiej8bo93brjjmj.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
+
+
+const PORT = 5000;
+
+// Middleware
+
+app.set('view engine', 'ejs');
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+app.get('/', (req, res)=>{
+    res.render('index');
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+app.get('/login', (req,res)=>{
+    res.render('login');
+})
+
+app.post('/login', (req,res)=>{
+    let token = req.body.token;
+
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+        const domain = payload['hd'];
+        console.log(payload);
+      }
+      verify()
+      .then(()=>{
+          res.cookie('session-token', token);
+          res.send('success')
+      })
+      .catch(console.error);
+
+})
+
+app.get('/housing', checkAuthenticated, (req, res)=>{
+    let user = req.user;
+    res.render('housing', {user});
+})
+
+app.get('/map', checkAuthenticated, (req, res)=>{
+    let user = req.user;
+    res.render('map', {user});
+})
+
+app.get('/profile', checkAuthenticated, (req, res)=>{
+    let user = req.user;
+    res.render('profile', {user});
+})
+
+app.get('/logout', (req, res)=>{
+    res.clearCookie('session-token');
+    res.redirect('/login')
+
+})
+
+
+function checkAuthenticated(req, res, next){
+
+    let token = req.cookies['session-token'];
+
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+      verify()
+      .then(()=>{
+          req.user = user;
+          next();
+      })
+      .catch(err=>{
+          res.redirect('/login')
+      })
+
+}
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
