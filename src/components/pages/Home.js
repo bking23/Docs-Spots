653defdb6072591b4e8a89639b4cc2a1d1d2b5db
John Richard <john.b.richard@outlook.com>
2021-03-10 21:40:10 -0500

    link map api to weather call add React .env vars

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Container} from 'reactstrap';
+import {React,useState} from 'react';
+import {Container,Button} from 'reactstrap';
 
 function Home(props){
   return(
--- src/components/pages/Nearby.js
+++ src/components/pages/Nearby.js
@@ -1,22 +1,71 @@
-import React from 'react';
-import {Container,Row,Col} from 'reactstrap';
+import {React,useState} from 'react';
+import {Button,Col,Container,Label,Input,Row} from 'reactstrap';
+import {GoogleMap, LoadScript} from '@react-google-maps/api';
 
 function Nearby(props){
+  const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+  var [location,setLocation]=useState({
+    city:"Towson",
+    coordinates:{
+      lat:39.4015,
+      lng:-76.6019
+    }
+  })
+  var [weather,setWeather]=useState({
+    temp:75,
+    desc:"Sunny"
+  })
+  var [forecast, setForecast]=useState({
+
+  })
+  var getWeather = () => {
+    var inputValue = document.querySelector('.inputValue');
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=+${inputValue.value}+&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      setLocation({
+        city:data['name'],
+        coordinates:{
+          lat:data['coord']['lat'],
+          lng:data['coord']['lon']
+        }
+      })
+      setWeather({
+        temp:((data['main']['temp'] - 273.15)* 1.8000 + 32.00).toFixed(2), //converts from K to F
+        desc:data['weather'][0]['description']
+      })
+    })
+    .catch(err => alert("An error occurred!"))
+  }
   return(
-    <Container>
-      <h1>Near You</h1>
+    <Container className="App-content">
+      <h1>Nearby</h1>
       <Container>
-        <Row>
-          <p align="center">This website is under construction! Check in later for updates!</p>
-        </Row>
-        <Row>
-          <Col>
-            <iframe width="500" height="400" frameborder="0"
-            src="https://www.bing.com/maps/embed?h=400&w=500&cp=39.398698652149086~-76.6038566685178&lvl=14&typ=d&sty=r&src=SHELL&FORM=MBEDV8" 
-            scrolling="no">
-            </iframe>
-          </Col>
-        </Row>
+        <Container>
+          <p align="center">Enter a city to find out its current weather status.</p>
+          <Container className="display">
+            <Row>
+              <Col>
+                <Label for="inputValue">Enter a city:</Label>
+                <Input type="text" className="inputValue" placeholder={location.city}/><br/>
+                <Button type="submit" value="submit" color="primary" onClick={getWeather}>Submit</Button>
+              </Col>
+              <Col>
+                <h6>{location.city}</h6>
+                <p>{weather.temp}&#176; F</p>
+                <p>{weather.desc}</p>
+              </Col>
+              <Col>
+                <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
+                  <GoogleMap mapContainerStyle={containerStyle} zoom={15} center={location.coordinates}/>
+                </LoadScript>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
       </Container>
     </Container>
   );
